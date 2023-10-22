db5ff34480712ecdaf8d30ec2a88f19c41683f3c
Alex Speranza <alex.speranza@studio.unibo.it>
2023-10-22 16:16:30 +0200

    feat: add a  command issuer

---
+++ src/lib/common/command_issuer.rs
@@ -0,0 +1,20 @@
+use std::process::{Output, Command};
+use crate::common::commons::print_error_and_exit;
+
+#[derive(Debug)]
+pub struct CommandIssuer { }
+
+impl CommandIssuer {
+    pub fn run(executable: &str, args: Vec<&str>) -> Output {
+        Command::new(executable)
+                .args(&args)
+                .output()
+                .unwrap_or_else( |e| {
+                    print_error_and_exit(&format!("Failed to run command '{}': {}", &args.join(" "), e));
+                })
+    }
+
+    pub fn git(args: Vec<&str>) -> Output {
+        CommandIssuer::run("git", args)
+    }
+}
