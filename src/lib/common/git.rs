ff80578e6255b853197f85dac9dcd87f6480aef3
Alex Speranza <alex.speranza@studio.unibo.it>
2023-10-23 15:33:36 +0200

    feat: add function to retrieve the git directory

--- src/lib/common/git.rs
+++ src/lib/common/git.rs
@@ -1,5 +1,14 @@
 use crate::common::command_issuer::CommandIssuer;
 
+use super::commons::print_error_and_exit;
+
 pub fn is_in_git_repository() -> bool {
     CommandIssuer::git(vec!("rev-parse", "--is-inside-work-tree")).status.success()
+}
+
+pub fn git_dir() -> String {
+    match std::str::from_utf8(&CommandIssuer::git(vec!["rev-parse", "--absolute-git-dir"]).stdout) {
+        Ok(path) => path.trim().to_string(),
+        Err(e) => print_error_and_exit(&e.to_string()),
+    }
 }
