fa2cab93111fb968bc3cd520b0db4918d7929ba0
Alex Speranza <alex.speranza@studio.unibo.it>
2023-10-23 15:20:13 +0200

    feat: add functions to read the lines of a file and to append a line to a file

--- src/lib/common/commons.rs
+++ src/lib/common/commons.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 pub fn print_error_and_exit(message: &str) -> ! {
     eprintln!("{}", message);
     std::process::exit(1)
@@ -13,3 +15,20 @@ pub fn print_cli_error_message_and_exit(stderr: &Vec<u8>, what: &str) -> ! {
     )
 }
 
+pub fn read_lines(path: &std::path::Path) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let read_result = std::fs::read(path);
+    match read_result {
+        Ok(content) => {
+            match std::string::String::from_utf8(content) {
+                Ok(s) => Ok(s.split("\n").filter(|s| !s.is_empty()).map(|line| line.to_string()).collect()),
+                Err(e) => Err(Box::new(e)),
+            }
+        },
+        Err(e) => Err(Box::new(e)),
+    }
+}
+
+pub fn append_line(path: &str, line: &str) -> std::io::Result<()> {
+    let mut f = std::fs::File::options().append(true).open(path).unwrap();
+    write!(f, "\n{}", line)
+}
