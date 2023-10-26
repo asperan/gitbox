af2f0145ae45382024bdbef0eea54d57120055fd
Alex Speranza <alex.speranza@studio.unibo.it>
2023-10-26 10:52:16 +0200

    feat: add options to describe command

--- src/lib/subcommands/describe.rs
+++ src/lib/subcommands/describe.rs
@@ -12,7 +12,33 @@ use self::docker::DescribeDockerSubCommand;
 #[command(about = "Calculate the next version")]
 pub struct DescribeSubCommand {
     #[command(subcommand)]
-    subcommand: Option<DescribeSubCommands>
+    subcommand: Option<DescribeSubCommands>,
+
+    #[arg(long, help = "Describe a prerelease")]
+    prerelease: bool,
+
+    #[arg(long, help = "Set the pattern for the new prerelease. A number can be used with the placeholder '%d'", requires("prerelease"))]
+    prerelease_pattern: Option<String>,
+    #[arg(long, help = "Set the pattern of the old prerelease. Uses the same placeholder as '--prerelease-pattern'. Use this option when changing prerelease pattern", requires("prerelease"))]
+    old_prerelease_pattern: Option<String>,
+
+    #[arg(short, long, help = "Print the last version (if possible) in addition to the new version")]
+    diff: bool,
+
+    #[arg(short, long, help = "Add a metadata to include in the new version (can be used multiple times)")]
+    metadata: Vec<String>,
+
+    #[arg(long, help = "Set the expression which triggers a major change")]
+    major_trigger: Option<String>,
+    #[arg(long, help = "Set the expression which triggers a minor change")]
+    minor_trigger: Option<String>,
+    #[arg(long, help = "Set the expression which triggers a patch change")]
+    patch_trigger: Option<String>,
+
+    #[arg(short = 't', long, help = "Create a new signed tag with the computed version")]
+    create_tag: bool,
+    #[arg(short = 'M', long, help = "Set the additional message for the created tag", requires("create_tag"))]
+    tag_message: Option<String>,
 }
 
 impl DescribeSubCommand {
