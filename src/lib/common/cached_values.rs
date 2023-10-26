7797bc2c013a6037c304ba7e778b438ac1f52498
Alex Speranza <alex.speranza@studio.unibo.it>
2023-10-26 18:37:19 +0200

    feat: add the used regexs and last version and release to cached values

--- src/lib/common/cached_values.rs
+++ src/lib/common/cached_values.rs
@@ -1,4 +1,6 @@
-use super::git::git_dir;
+use regex::Regex;
+
+use super::git::{git_dir, last_version, last_release, FULL_SEMANTIC_VERSION_PATTERN, CONVENTIONAL_COMMIT_PATTERN};
 
 pub struct CachedValues {}
 
@@ -14,6 +16,58 @@ impl CachedValues {
             }
         }
     }
+
+    pub fn last_version() -> &'static Option<String> {
+        unsafe {
+            match &LAST_VERSION {
+                Some(value) => value,
+                None => {
+                    LAST_VERSION = Some(last_version());
+                    LAST_VERSION.as_ref().unwrap()
+                },
+            }
+        }
+    }
+
+    pub fn last_release() -> &'static Option<String> {
+        unsafe {
+            match &LAST_RELEASE {
+                Some(value) => value,
+                None => {
+                    LAST_RELEASE = Some(last_release());
+                    LAST_RELEASE.as_ref().unwrap()
+                },
+            }
+        }
+    }
+
+    pub fn semantic_version_regex() -> &'static Regex {
+        unsafe {
+            match &SEMANTIC_VERSION_REGEX {
+                Some(value) => value,
+                None => {
+                    SEMANTIC_VERSION_REGEX = Some(Regex::new(FULL_SEMANTIC_VERSION_PATTERN).unwrap());
+                    SEMANTIC_VERSION_REGEX.as_ref().unwrap()
+                },
+            }
+        }
+    }
+
+    pub fn conventional_commit_regex() -> &'static Regex {
+        unsafe {
+            match &CONVENTIONAL_COMMIT_REGEX {
+                Some(value) => value,
+                None => {
+                    CONVENTIONAL_COMMIT_REGEX = Some(Regex::new(CONVENTIONAL_COMMIT_PATTERN).unwrap());
+                    CONVENTIONAL_COMMIT_REGEX.as_ref().unwrap()
+                },
+            }
+        }
+    }
 }
 
 static mut GIT_DIR: Option<String> = None;
+static mut LAST_VERSION: Option<Option<String>> = None;
+static mut LAST_RELEASE: Option<Option<String>> = None;
+static mut CONVENTIONAL_COMMIT_REGEX: Option<Regex> = None;
+static mut SEMANTIC_VERSION_REGEX: Option<Regex> = None;
