d98272fd1b3cd919798aed89b5f54c11d1d7e5cb
Alex Speranza <alex.speranza@studio.unibo.it>
2023-10-26 18:35:37 +0200

    refactor: change the command issuer functions to accept string slices

--- src/lib/common/command_issuer.rs
+++ src/lib/common/command_issuer.rs
@@ -5,16 +5,16 @@ use crate::common::commons::print_error_and_exit;
 pub struct CommandIssuer { }
 
 impl CommandIssuer {
-    pub fn run(executable: &str, args: Vec<&str>) -> Output {
+    pub fn run(executable: &str, args: &[&str]) -> Output {
         Command::new(executable)
-                .args(&args)
+                .args(args)
                 .output()
                 .unwrap_or_else( |e| {
                     print_error_and_exit(&format!("Failed to run command '{}': {}", &args.join(" "), e));
                 })
     }
 
-    pub fn git(args: Vec<&str>) -> Output {
+    pub fn git(args: &[&str]) -> Output {
         CommandIssuer::run("git", args)
     }
 }
