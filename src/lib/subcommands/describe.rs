099ddddb96fd35d4b2e65b75097e8989d0b93bc0
Alex Speranza <alex.speranza@studio.unibo.it>
2023-10-27 17:00:55 +0200

    feat: implement the full version description

--- src/lib/subcommands/describe.rs
+++ src/lib/subcommands/describe.rs
@@ -2,10 +2,11 @@ mod docker;
 mod change_parser;
 mod stable;
 mod metadata;
+mod prerelease;
 
 use clap::{Args, Subcommand};
 
-use crate::{subcommands::describe::{stable::StableVersionCalculator, metadata::MetadataGenerator}, common::{cached_values::CachedValues, semantic_version::SemanticVersion, commons::print_error_and_exit}};
+use crate::{subcommands::describe::{stable::StableVersionCalculator, metadata::MetadataGenerator, prerelease::PrereleaseUpdater}, common::{cached_values::CachedValues, semantic_version::SemanticVersion, commons::print_error_and_exit}};
 
 use self::{docker::DescribeDockerSubCommand, metadata::MetadataSpecs};
 
@@ -19,10 +20,10 @@ pub struct DescribeSubCommand {
     #[arg(long, help = "Describe a prerelease")]
     prerelease: bool,
 
-    #[arg(long, help = "Set the pattern for the new prerelease. A number can be used with the placeholder '%d'", requires("prerelease"))]
-    prerelease_pattern: Option<String>,
-    #[arg(long, help = "Set the pattern of the old prerelease. Uses the same placeholder as '--prerelease-pattern'. Use this option when changing prerelease pattern", requires("prerelease"))]
-    old_prerelease_pattern: Option<String>,
+    #[arg(long, help = "Set the pattern for the new prerelease. A number can be used with the placeholder '%d'", requires("prerelease"), default_value = "dev%d")]
+    prerelease_pattern: String,
+    #[arg(long, help = "Set the pattern of the old prerelease. Uses the same placeholder as '--prerelease-pattern'. Use this option when changing prerelease pattern", requires("prerelease"), default_value = "dev%d")]
+    old_prerelease_pattern: String,
 
     #[arg(short, long, help = "Print the last version (if possible) in addition to the new version")]
     diff: bool,
@@ -56,33 +57,22 @@ impl DescribeSubCommand {
     }
 
     fn update_version(&self) -> SemanticVersion {
-        /*
-        println!("Basic describe called");
-        let test_trigger = "scope IN [core-deps, frontend] AND type IN [ test, feat ] OR breaking";
-        // let test_trigger = "type IN [ test, feat ]";
-        // let test_trigger = "breaking";
-        // let test_trigger = "scope IN [ core-deps, backend ]";
-        let trigger = ChangeTriggerParser::run(test_trigger);
-        dbg!(&trigger);
-        dbg!(trigger.call("test", &Some("backend".to_string()), false));
-        */
-        dbg!(CachedValues::last_version());
-        dbg!(CachedValues::last_stable_release());
         let stable_updater = StableVersionCalculator::new(&self.major_trigger, &self.minor_trigger, &self.patch_trigger);
-        let new_stable_version = stable_updater.next_stable(CachedValues::last_stable_release());
-
-        // TODO: calc prerelease (needs last_version and new_stable_version), returns a Option? and
-        // starts as None?
-        dbg!(&self.metadata);
-        let metadata_str = MetadataGenerator::generate(&self.metadata);
-        dbg!(&metadata_str);
-        // TODO: final version: format!("{}{}{}", new_stable_version, prerelease.map_or(|p|
-        // format!("-{}", p), ""), metadata.map_or(|m| format!("+{}", m), "")
-        if CachedValues::last_stable_release().as_ref().is_some_and(|v| new_stable_version == *v) {
+        let mut new_version = stable_updater.next_stable(CachedValues::last_stable_release());
+        if CachedValues::last_stable_release().as_ref().is_some_and(|v| new_version == *v) {
             print_error_and_exit("There are no relevant changes since the last stable version. Change triggers or commit some relevant changes to describe a new version.")
         }
-        dbg!(&new_stable_version);
-        new_stable_version
+
+        let prerelease = if self.prerelease {
+            let prerelease_updater = PrereleaseUpdater::new(&self.prerelease_pattern, &self.old_prerelease_pattern);
+            Some(prerelease_updater.update_prerelease(&new_version, CachedValues::last_version()))
+        } else {
+            None
+        };
+        let metadata_str = MetadataGenerator::generate(&self.metadata);
+        new_version.add_prerelease(prerelease);
+        new_version.add_metadata(metadata_str);
+        new_version
     }
 
     fn print_version(&self, new_version: &SemanticVersion, old_version: Option<&SemanticVersion>) {
