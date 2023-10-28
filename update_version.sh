e9223935ede04e19f8bdc4dbf4ca641929cdfd3e
Alex Speranza <alex.speranza@studio.unibo.it>
2023-10-28 16:56:23 +0200

    build: add script to synchronize versions in Cargo.toml and lib.rs

---
+++ update_version.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+if [ "$#" -lt 1 ]; then
+    echo "This script requires 1 argument, which is the new version to use"
+    exit 1
+fi
+
+# Update version in Cargo.toml
+sed -E -i "4,4 s/(version = \")(.*)(\")/\\1$1\\3/" Cargo.toml
+
+# Update version in src/lib/lib.rs
+sed -E -i "10,10 s/(\\#\\[command\\(version = \")(.*)(\"\\)\\])/\1$1\3/" src/lib/lib.rs
+
