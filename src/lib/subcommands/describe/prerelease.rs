0b0561c3d6b3c45d734c51a2aec68faca9912bc5
Alex Speranza <alex.speranza@studio.unibo.it>
2023-11-08 12:06:25 +0100

    fix: fix panic on wrong prerelease pattern

--- src/lib/subcommands/describe/prerelease.rs
+++ src/lib/subcommands/describe/prerelease.rs
@@ -44,16 +44,24 @@ impl PrereleaseUpdater {
             self.new_pattern.replace("%d", "1")
         } else {
             let old_pattern_regex = Regex::new(&self.old_pattern.replace("%d", "(\\d+)")).unwrap();
-            let number: u16 = old_pattern_regex
-                .captures(old_version.as_ref().unwrap().prerelease().as_ref().unwrap())
-                .unwrap()
-                .get(1)
-                .unwrap()
-                .as_str()
-                .parse::<u16>()
-                .unwrap()
-                + 1;
-            self.new_pattern.replace("%d", &number.to_string())
+            let prerelease = old_version.as_ref().unwrap().prerelease().as_ref().unwrap();
+            match old_pattern_regex
+                .captures(prerelease) {
+                    Some(caps) => {
+                        let number: u16 = caps
+                            .get(1)
+                            .unwrap()
+                            .as_str()
+                            .parse::<u16>()
+                            .unwrap()
+                            + 1;
+                        self.new_pattern.replace("%d", &number.to_string())
+
+                    },
+                    None => {
+                        print_error_and_exit(&format!("Failed to match prerelease '{}' with pattern '{}'", prerelease, self.new_pattern));
+                    }
+                }
         }
     }
 }
