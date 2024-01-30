3d3b5763d0185d6ac804a81329b15f7155732c25
Alex Speranza <alex.speranza@studio.unibo.it>
2024-01-30 14:24:38 +0100

    refactor(usecase): change return type of repository methods into Result

--- src/lib/usecases/repository/version_repository.rs
+++ src/lib/usecases/repository/version_repository.rs
@@ -1,6 +1,6 @@
-use crate::domain::semantic_version::SemanticVersion;
+use crate::domain::{semantic_version::SemanticVersion, type_aliases::AnyError};
 
 pub trait VersionRepository {
-    fn last_version(&self) -> Option<SemanticVersion>;
-    fn last_stable_version(&self) -> Option<SemanticVersion>;
+    fn last_version(&self) -> Result<Option<SemanticVersion>, AnyError>;
+    fn last_stable_version(&self) -> Result<Option<SemanticVersion>, AnyError>;
 }
