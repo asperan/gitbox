3691fc36d602fe3a167ecde34ef55246159f424a
Alex Speranza <alex.speranza@studio.unibo.it>
2024-01-31 11:47:46 +0100

    refactor(infrastructure): add error for failed command executions

---
+++ src/lib/infrastructure/error/command_execution_error.rs
@@ -0,0 +1,40 @@
+use std::{error::Error, fmt::Display};
+
+#[derive(Debug)]
+pub struct CommandExecutionError {
+    full_command: String,
+    cause: Box<dyn Error>,
+}
+
+impl CommandExecutionError {
+    pub fn new(full_command: &str, cause: Box<dyn Error>) -> CommandExecutionError {
+        CommandExecutionError {
+            full_command: full_command.to_owned(),
+            cause,
+        }
+    }
+}
+
+impl Display for CommandExecutionError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Failed to run command '{}': {}",
+            self.full_command, self.cause
+        )
+    }
+}
+
+impl Error for CommandExecutionError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        Some(self.cause.as_ref())
+    }
+
+    fn description(&self) -> &str {
+        "description() is deprecated; use Display"
+    }
+
+    fn cause(&self) -> Option<&dyn Error> {
+        self.source()
+    }
+}
