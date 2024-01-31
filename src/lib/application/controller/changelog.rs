88afdba43ac2db89b4841baed81ba72df62be3f2
Alex Speranza <alex.speranza@studio.unibo.it>
2024-01-31 13:55:07 +0100

    refactor(application): add tests to changelog controller

--- src/lib/application/controller/changelog.rs
+++ src/lib/application/controller/changelog.rs
@@ -83,3 +83,159 @@ impl ChangelogController {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{error::Error, fmt::Display, rc::Rc};
+
+    use crate::{
+        application::{
+            controller::exit_code::ControllerExitCode,
+            manager::output_manager::OutputManager,
+            options::changelog::ChangelogOptions,
+            retriever::{commit_retriever::CommitRetriever, version_retriever::VersionRetriever},
+        },
+        domain::{semantic_version::SemanticVersion, type_aliases::AnyError},
+    };
+
+    use super::ChangelogController;
+
+    struct MockCommitRetriever {}
+    impl CommitRetriever for MockCommitRetriever {
+        fn get_all_commits(&self) -> Result<Box<dyn DoubleEndedIterator<Item = String>>, AnyError> {
+            unreachable!()
+        }
+
+        fn get_commits_from(
+            &self,
+            _version: &Option<SemanticVersion>,
+        ) -> Result<Box<dyn DoubleEndedIterator<Item = String>>, AnyError> {
+            Ok(Box::new(
+                vec![
+                    "feat: test".to_owned(),
+                    "fix: test".to_owned(),
+                    "test: test".to_owned(),
+                ]
+                .into_iter(),
+            ))
+        }
+    }
+
+    #[derive(Debug)]
+    struct MockVersionError {}
+    impl Display for MockVersionError {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "MockVersionError")
+        }
+    }
+
+    impl Error for MockVersionError {
+        fn source(&self) -> Option<&(dyn Error + 'static)> {
+            None
+        }
+
+        fn description(&self) -> &str {
+            "description() is deprecated; use Display"
+        }
+
+        fn cause(&self) -> Option<&dyn Error> {
+            self.source()
+        }
+    }
+
+    struct MockVersionRetriever {}
+    impl VersionRetriever for MockVersionRetriever {
+        fn last_version(&self) -> Result<Option<String>, AnyError> {
+            Err(Box::new(MockVersionError {}))
+        }
+
+        fn last_stable_version(&self) -> Result<Option<String>, AnyError> {
+            Ok(Some("0.1.0".to_owned()))
+        }
+    }
+
+    struct MockOutputManager {}
+    impl OutputManager for MockOutputManager {
+        fn output(&self, _message: &str) {}
+
+        fn error(&self, _error: &str) {}
+    }
+
+    #[test]
+    fn wrong_trigger_exits_with_error() {
+        let options = ChangelogOptions::new(
+            false,
+            String::from("%s"),
+            String::from("%s"),
+            String::from("%s"),
+            String::from("%s"),
+            String::from("%s"),
+            String::from("%s"),
+            Some("abc".to_string()),
+        )
+        .expect("Changelog options should be correct");
+        let commit_retriever = MockCommitRetriever {};
+        let version_retriever = MockVersionRetriever {};
+        let output_manager = MockOutputManager {};
+        let controller = ChangelogController::new(
+            options,
+            Rc::new(commit_retriever),
+            Rc::new(version_retriever),
+            Rc::new(output_manager),
+        );
+        let result = controller.changelog();
+        assert!(matches!(result, ControllerExitCode::Error(..)));
+    }
+
+    #[test]
+    fn correct_usecase_execution() {
+        let options = ChangelogOptions::new(
+            false,
+            String::from("%s"),
+            String::from("%s"),
+            String::from("%s"),
+            String::from("%s"),
+            String::from("%s"),
+            String::from("%s"),
+            None,
+        )
+        .expect("Changelog options should be correct");
+        let commit_retriever = MockCommitRetriever {};
+        let version_retriever = MockVersionRetriever {};
+        let output_manager = MockOutputManager {};
+        let controller = ChangelogController::new(
+            options,
+            Rc::new(commit_retriever),
+            Rc::new(version_retriever),
+            Rc::new(output_manager),
+        );
+        let result = controller.changelog();
+        assert!(matches!(result, ControllerExitCode::Ok));
+    }
+
+    #[test]
+    fn failed_execution_of_usecase() {
+        let options = ChangelogOptions::new(
+            true,
+            String::from("%s"),
+            String::from("%s"),
+            String::from("%s"),
+            String::from("%s"),
+            String::from("%s"),
+            String::from("%s"),
+            None,
+        )
+        .expect("Changelog options should be correct");
+        let commit_retriever = MockCommitRetriever {};
+        let version_retriever = MockVersionRetriever {};
+        let output_manager = MockOutputManager {};
+        let controller = ChangelogController::new(
+            options,
+            Rc::new(commit_retriever),
+            Rc::new(version_retriever),
+            Rc::new(output_manager),
+        );
+        let result = controller.changelog();
+        assert!(matches!(result, ControllerExitCode::Error(..)));
+    }
+}
