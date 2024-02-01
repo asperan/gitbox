1d762b38f7c56f6d680101d06ad1183f2ba68058
Alex Speranza <alex.speranza@studio.unibo.it>
2024-02-01 12:41:23 +0100

    refactor(application): change argument type for CommitManager trait methods

--- src/lib/application/manager/commit_manager.rs
+++ src/lib/application/manager/commit_manager.rs
@@ -1,7 +1,7 @@
-use crate::{domain::conventional_commit::ConventionalCommit, usecases::type_aliases::AnyError};
+use crate::usecases::type_aliases::AnyError;
 
 pub trait CommitManager {
-    fn create_commit(&self, commit: ConventionalCommit) -> Result<(), AnyError>;
+    fn create_commit(&self, commit: &str) -> Result<(), AnyError>;
 
-    fn create_empty_commit(&self, commit: ConventionalCommit) -> Result<(), AnyError>;
+    fn create_empty_commit(&self, commit: &str) -> Result<(), AnyError>;
 }
