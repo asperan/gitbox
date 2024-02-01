2ce336d3ebc985831deaab6371c9d173d27e3dcb
Alex Speranza <alex.speranza@studio.unibo.it>
2024-02-01 21:19:21 +0100

    refactor(application): add controller for Refresh

--- src/lib/application/controller.rs
+++ src/lib/application/controller.rs
@@ -2,3 +2,4 @@ pub mod changelog;
 pub mod commit;
 pub mod exit_code;
 pub mod init;
+pub mod refresh;
---
+++ src/lib/application/controller/refresh.rs
@@ -0,0 +1,57 @@
+use std::rc::Rc;
+
+use crate::{
+    application::{
+        commit_summary_repository_impl::CommitSummaryRepositoryImpl,
+        gitextra_write_repository_impl::GitExtraWriteRepositoryImpl,
+        manager::{gitextra_write_manager::GitExtraWriteManager, output_manager::OutputManager},
+        retriever::commit_retriever::CommitRetriever,
+    },
+    usecases::usecases::{
+        refresh_types_and_scopes::RefreshTypesAndScopesUseCase, usecase::UseCase,
+    },
+};
+
+use super::exit_code::ControllerExitCode;
+
+pub struct RefreshController {
+    commit_retriever: Rc<dyn CommitRetriever>,
+    gitextra_write_manager: Rc<dyn GitExtraWriteManager>,
+    output_manager: Rc<dyn OutputManager>,
+}
+
+impl RefreshController {
+    pub fn new(
+        commit_retriever: Rc<dyn CommitRetriever>,
+        gitextra_write_manager: Rc<dyn GitExtraWriteManager>,
+        output_manager: Rc<dyn OutputManager>,
+    ) -> RefreshController {
+        RefreshController {
+            commit_retriever,
+            gitextra_write_manager,
+            output_manager,
+        }
+    }
+
+    pub fn refresh(&self) -> ControllerExitCode {
+        let usecase = RefreshTypesAndScopesUseCase::new(
+            Rc::new(CommitSummaryRepositoryImpl::new(
+                self.commit_retriever.clone(),
+            )),
+            Rc::new(GitExtraWriteRepositoryImpl::new(
+                self.gitextra_write_manager.clone(),
+            )),
+        );
+        match usecase.execute() {
+            Ok(_) => {
+                self.output_manager
+                    .output("Commit types and scopes refreshed");
+                ControllerExitCode::Ok
+            }
+            Err(e) => {
+                self.output_manager.error(&e.to_string());
+                ControllerExitCode::Error(1)
+            }
+        }
+    }
+}
