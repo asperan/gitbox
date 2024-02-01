b30ebef0c398453bf318d7e09cc1f47c7ec8faa5
Alex Speranza <alex.speranza@studio.unibo.it>
2024-02-01 21:22:56 +0100

    refactor(infrastructure): add subcommand commit refresh

---
+++ src/lib/infrastructure/subcommands/commit_refresh.rs
@@ -0,0 +1,30 @@
+use clap::Args;
+use std::rc::Rc;
+
+use crate::{
+    application::controller::{exit_code::ControllerExitCode, refresh::RefreshController},
+    infrastructure::{
+        git_cli::GitCli, gitextra_manager_impl::GitExtraManagerImpl,
+        output_manager_impl::OutputManagerImpl, subcommand::Subcommand,
+    },
+};
+
+#[derive(Args, Clone, Debug)]
+pub struct RefreshTypesAndScopesSubcommand {}
+
+impl Subcommand for RefreshTypesAndScopesSubcommand {
+    fn execute(&self) -> i32 {
+        let git_cli = Rc::new(GitCli::new());
+        let gitextra_manager = Rc::new(GitExtraManagerImpl::new(git_cli.clone()));
+        let output_manager = Rc::new(OutputManagerImpl::new());
+        let controller = RefreshController::new(
+            git_cli.clone(),
+            gitextra_manager.clone(),
+            output_manager.clone(),
+        );
+        match controller.refresh() {
+            ControllerExitCode::Ok => 0,
+            ControllerExitCode::Error(i) => i,
+        }
+    }
+}
