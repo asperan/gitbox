01d75cf1795c36b57338f639b4a271c62501da19
Alex Speranza <alex.speranza@studio.unibo.it>
2024-02-01 12:24:54 +0100

    refactor(application): rename CommitRepositoryImpl into CommitSummaryRepositoryImpl

---
+++ src/lib/application/commit_summary_repository_impl.rs
@@ -0,0 +1,93 @@
+use std::{rc::Rc, str::FromStr};
+
+use crate::{
+    application::retriever::commit_retriever::CommitRetriever,
+    domain::{commit::CommitSummary, semantic_version::SemanticVersion},
+    usecases::{
+        repository::commit_summary_repository::CommitSummaryRepository, type_aliases::AnyError,
+    },
+};
+
+pub struct CommitSummaryRepositoryImpl {
+    commit_retriever: Rc<dyn CommitRetriever>,
+}
+
+impl CommitSummaryRepositoryImpl {
+    pub fn new(commit_retriever: Rc<dyn CommitRetriever>) -> CommitSummaryRepositoryImpl {
+        CommitSummaryRepositoryImpl { commit_retriever }
+    }
+}
+
+impl CommitSummaryRepository for CommitSummaryRepositoryImpl {
+    fn get_all_commits(
+        &self,
+    ) -> Result<Box<dyn DoubleEndedIterator<Item = CommitSummary>>, AnyError> {
+        let commit_list = self.commit_retriever.get_all_commits()?;
+        Ok(Box::new(commit_list.map(|c| {
+            CommitSummary::from_str(&c).expect("Commit deserialization cannot fail")
+        })))
+    }
+
+    fn get_commits_from(
+        &self,
+        version: &Option<SemanticVersion>,
+    ) -> Result<Box<dyn DoubleEndedIterator<Item = CommitSummary>>, AnyError> {
+        let commit_list = self.commit_retriever.get_commits_from(version)?;
+        Ok(Box::new(commit_list.map(|c| {
+            CommitSummary::from_str(&c).expect("Commit deserialization cannot fail")
+        })))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use std::rc::Rc;
+
+    use crate::{
+        application::retriever::commit_retriever::CommitRetriever,
+        domain::{commit::CommitSummary, semantic_version::SemanticVersion},
+        usecases::{
+            repository::commit_summary_repository::CommitSummaryRepository, type_aliases::AnyError,
+        },
+    };
+
+    use super::CommitSummaryRepositoryImpl;
+
+    struct MockCommitRetriever {}
+
+    impl CommitRetriever for MockCommitRetriever {
+        fn get_all_commits(&self) -> Result<Box<dyn DoubleEndedIterator<Item = String>>, AnyError> {
+            Ok(Box::new(vec!["test freeform", "feat: im conventional"].into_iter().map(|it| it.to_string())))
+        }
+
+        fn get_commits_from(
+            &self,
+            _version: &Option<SemanticVersion>,
+        ) -> Result<Box<dyn DoubleEndedIterator<Item = String>>, AnyError> {
+            Ok(Box::new(vec!["test freeform", "feat: im conventional"].into_iter().map(|it| it.to_string())))
+        }
+    }
+
+    #[test]
+    fn get_all_commits_basic() {
+        let repository = CommitSummaryRepositoryImpl::new(Rc::new(MockCommitRetriever {}));
+        let commit_list = repository.get_all_commits();
+        assert!(commit_list.is_ok());
+        assert!(commit_list
+            .expect("Just asserted its OK-ness")
+            .all(|it| matches!(it, CommitSummary::Conventional(..))
+                || matches!(it, CommitSummary::FreeForm(..))));
+    }
+
+    #[test]
+    fn get_commits_from_basic() {
+        let repository = CommitSummaryRepositoryImpl::new(Rc::new(MockCommitRetriever {}));
+        let commit_list = repository.get_commits_from(&None);
+        assert!(commit_list.is_ok());
+        assert!(commit_list
+            .expect("Just asserted its OK-ness")
+            .all(|it| matches!(it, CommitSummary::Conventional(..))
+                || matches!(it, CommitSummary::FreeForm(..))))
+    }
+}
