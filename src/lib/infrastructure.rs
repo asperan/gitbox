bcfc6822bba4b0ee994e64531b9c97af80476eea
Alex Speranza <alex.speranza@studio.unibo.it>
2024-02-01 22:41:26 +0100

    refactor(infrastructure): add GitExtraReadManager trait

--- src/lib/infrastructure.rs
+++ src/lib/infrastructure.rs
@@ -2,6 +2,7 @@ pub mod error;
 mod git_cli;
 mod gitextra_append_manager;
 mod gitextra_manager_impl;
+mod gitextra_read_manager;
 pub mod output_manager_impl;
 pub mod subcommand;
 pub mod subcommands;
---
+++ src/lib/infrastructure/gitextra_read_manager.rs
@@ -0,0 +1,6 @@
+use crate::usecases::type_aliases::AnyError;
+
+pub trait GitExtraReadManager {
+    fn get_types(&self) -> Result<Vec<String>, AnyError>;
+    fn get_scopes(&self) -> Result<Vec<String>, AnyError>;
+}
