0b80d81cf793ad49206c72c41258045304ddb257
Alex Speranza <alex.speranza@studio.unibo.it>
2024-02-01 12:00:44 +0100

    refactor(domain): add ConventionalCommit struct which has also a message body

--- src/lib/domain.rs
+++ src/lib/domain.rs
@@ -1,4 +1,5 @@
 pub mod commit;
+pub mod conventional_commit;
 pub mod conventional_commit_summary;
 pub mod semantic_version;
 pub mod trigger;
---
+++ src/lib/domain/conventional_commit.rs
@@ -0,0 +1,23 @@
+use super::conventional_commit_summary::ConventionalCommitSummary;
+
+pub struct ConventionalCommit {
+    summary: ConventionalCommitSummary,
+    message: Option<String>,
+}
+
+impl ConventionalCommit {
+    pub fn new(typ: String, scope: Option<String>, breaking: bool, summary: String, message: Option<String>) -> ConventionalCommit {
+        ConventionalCommit {
+            summary: ConventionalCommitSummary::new(typ, scope, breaking, summary),
+            message
+        }
+    }
+
+    pub fn summary(&self) -> &ConventionalCommitSummary {
+        &self.summary
+    }
+
+    pub fn message(&self) -> &Option<String> {
+        &self.message
+    }
+}
