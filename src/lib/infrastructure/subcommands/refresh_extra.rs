4ba6f99975e950ab0ac9c3205c5821c415675b69
Alex Speranza <alex.speranza@studio.unibo.it>
2024-02-05 17:37:56 +0100

    refactor(infrastructure): add check for being in a git dir

--- src/lib/infrastructure/subcommands/refresh_extra.rs
+++ src/lib/infrastructure/subcommands/refresh_extra.rs
@@ -2,7 +2,13 @@ use clap::Args;
 use std::rc::Rc;
 
 use crate::{
-    application::controller::{exit_code::ControllerExitCode, refresh::RefreshController},
+    application::{
+        controller::{exit_code::ControllerExitCode, refresh::RefreshController},
+        manager::{
+            gitinfo_ingress_manager::GitInfoIngressManager,
+            message_egress_manager::MessageEgressManager,
+        },
+    },
     infrastructure::{
         git_cli::GitCli, gitextra_manager_impl::GitExtraManagerImpl,
         output_manager_impl::OutputManagerImpl, subcommand::Subcommand,
@@ -16,8 +22,13 @@ pub struct RefreshExtraSubcommand {}
 impl Subcommand for RefreshExtraSubcommand {
     fn execute(&self) -> i32 {
         let git_cli = Rc::new(GitCli::new());
-        let gitextra_manager = Rc::new(GitExtraManagerImpl::new(git_cli.clone()));
         let output_manager = Rc::new(OutputManagerImpl::new());
+        if let Err(e) = git_cli.git_dir() {
+            output_manager.error(&format!("Failed to retrieve git dir: {}", e));
+            output_manager.error("refresh-extra subcommand can only be run inside a git project");
+            return 1;
+        }
+        let gitextra_manager = Rc::new(GitExtraManagerImpl::new(git_cli.clone()));
         let controller = RefreshController::new(
             git_cli.clone(),
             gitextra_manager.clone(),
