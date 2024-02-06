daafae841696ff70840ef0c734f095f1849a2755
Alex Speranza <alex.speranza@studio.unibo.it>
2024-02-06 19:38:53 +0100

    refactor(infrastructure): move old subcommand top-level module into subcommands infrastructure module

--- src/lib/infrastructure/subcommands.rs
+++ src/lib/infrastructure/subcommands.rs
@@ -1,10 +1,31 @@
-pub mod changelog;
-pub mod commit;
-pub mod complete;
-pub mod describe;
-pub mod grammar;
-pub mod init;
-pub mod license;
-pub mod refresh_extra;
-pub mod tree;
+use clap::Subcommand;
 
+use self::{
+    changelog::ChangelogSubCommand, commit::CommitSubCommand, complete::CompleteSubCommand,
+    describe::DescribeSubCommand, grammar::GrammarSubCommand, init::InitSubCommand,
+    license::LicenseSubCommand, refresh_extra::RefreshExtraSubcommand, tree::TreeSubCommand,
+};
+
+mod changelog;
+mod commit;
+mod complete;
+mod describe;
+mod grammar;
+mod init;
+mod license;
+mod refresh_extra;
+mod tree;
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    Changelog(ChangelogSubCommand),
+    Commit(CommitSubCommand),
+    Complete(CompleteSubCommand),
+    Describe(DescribeSubCommand),
+    Init(InitSubCommand),
+    License(LicenseSubCommand),
+    RefreshExtra(RefreshExtraSubcommand),
+    Tree(TreeSubCommand),
+    // HIDDEN COMMANDS
+    Grammar(GrammarSubCommand),
+}
