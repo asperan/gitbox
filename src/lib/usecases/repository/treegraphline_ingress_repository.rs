95e9d50b5681ab23def3f606352cb576db3bab35
Alex Speranza <alex.speranza@studio.unibo.it>
2024-02-07 12:34:51 +0100

    refactor(usecase): add repository trait to retrieve tree graph lines

---
+++ src/lib/usecases/repository/treegraphline_ingress_repository.rs
@@ -0,0 +1,5 @@
+use crate::usecases::{tree_graph_line::TreeGraphLine, type_aliases::AnyError};
+
+pub trait TreeGraphLineIngressRepository {
+    fn graph_lines(&self) -> Result<Box<[TreeGraphLine]>, AnyError>;
+}
