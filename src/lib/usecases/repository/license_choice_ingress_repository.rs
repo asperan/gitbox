3aa3a31573f6d3faf572d3b06e200d7ff2243d3c
Alex Speranza <alex.speranza@studio.unibo.it>
2024-02-07 16:17:07 +0100

    refactor(usecase): add lifetime to parameters

--- src/lib/usecases/repository/license_choice_ingress_repository.rs
+++ src/lib/usecases/repository/license_choice_ingress_repository.rs
@@ -1,5 +1,5 @@
 use crate::usecases::{license_metadata::LicenseMetadata, type_aliases::AnyError};
 
 pub trait LicenseChoiceIngressRepository {
-    fn ask_license(&self, list: &[LicenseMetadata]) -> Result<&LicenseMetadata, AnyError>;
+    fn ask_license<'a>(&self, list: &'a [LicenseMetadata]) -> Result<&'a LicenseMetadata, AnyError>;
 }
