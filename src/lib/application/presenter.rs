f0e2446cc6b97686ba0fdfc105af0c807ff2e170
Alex Speranza <alex.speranza@studio.unibo.it>
2024-02-07 12:37:16 +0100

    refactor(application): add implementation of FromStr for TreeGraphLine

--- src/lib/application/presenter.rs
+++ src/lib/application/presenter.rs
@@ -1,3 +1,4 @@
 mod commit_presenter;
 mod semantic_version_presenter;
+mod treegraphline_presenter;
 mod trigger_presenter;
---
+++ src/lib/application/presenter/treegraphline_presenter.rs
@@ -0,0 +1,29 @@
+use std::str::FromStr;
+
+use crate::{
+    application::error::treegraphline_format_error::TreeGraphLineFormatError,
+    usecases::{tree_graph_line::TreeGraphLine, type_aliases::AnyError},
+};
+
+impl FromStr for TreeGraphLine {
+    type Err = AnyError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // if the separator are 3, the resulting array will have 4 values:
+        // "1sep2sep3sep4" => [1, 2, 3, 4]
+        if s.matches(TreeGraphLine::separator()).count() == 3 {
+            let raw_array: Box<[&str]> = s.split(TreeGraphLine::separator()).collect();
+            Ok(TreeGraphLine::new(
+                raw_array[1].to_owned(),
+                raw_array[0].to_owned(),
+                raw_array[2].to_owned(),
+                raw_array[3].to_owned(),
+            ))
+        } else {
+            Err(Box::new(TreeGraphLineFormatError::new(&format!(
+                "the line must contain TreeGraphLine separator '{}' 3 times",
+                TreeGraphLine::separator()
+            ))))
+        }
+    }
+}
