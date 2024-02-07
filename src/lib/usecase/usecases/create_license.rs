4f64e36fdf955ff6fdbedfbe28468029dc8ced09
Alex Speranza <alex.speranza@studio.unibo.it>
2024-02-07 22:12:49 +0100

    test(usecase): add test to license usecase

--- src/lib/usecase/usecases/create_license.rs
+++ src/lib/usecase/usecases/create_license.rs
@@ -48,8 +48,81 @@ impl UseCase<()> for CreateLicenseUseCase {
 
 #[cfg(test)]
 mod tests {
+    use std::{cell::RefCell, rc::Rc};
+
+    use crate::usecase::{
+        license_metadata::LicenseMetadata,
+        repository::{
+            license_choice_ingress_repository::LicenseChoiceIngressRepository,
+            license_list_ingress_repository::LicenseListIngressRepository,
+            license_text_egress_repository::LicenseTextEgressRepository,
+            license_text_ingress_repository::LicenseTextIngressRepository,
+        },
+        type_aliases::AnyError,
+        usecases::{create_license::CreateLicenseUseCase, usecase::UseCase},
+    };
+
+    struct MockLicenseListIngressRepository {}
+    impl LicenseListIngressRepository for MockLicenseListIngressRepository {
+        fn license_list(&self) -> Result<Box<[LicenseMetadata]>, AnyError> {
+            Ok(Box::new([
+                LicenseMetadata::new("MIT", "mit-license"),
+                LicenseMetadata::new("MPL 2.0", "mpl-2.0"),
+            ]))
+        }
+    }
+
+    struct MockLicenseChoiceIngressRepository {}
+    impl LicenseChoiceIngressRepository for MockLicenseChoiceIngressRepository {
+        fn ask_license<'a>(
+            &self,
+            list: &'a [LicenseMetadata],
+        ) -> Result<&'a LicenseMetadata, AnyError> {
+            Ok(&list[0])
+        }
+    }
+
+    struct MockLicenseTextIngressRepository {}
+    impl LicenseTextIngressRepository for MockLicenseTextIngressRepository {
+        fn text(&self, license: &LicenseMetadata) -> Result<Box<str>, AnyError> {
+            Ok(format!(
+                "Name: {}\nReference: {}\n",
+                license.name(),
+                license.reference()
+            )
+            .into_boxed_str())
+        }
+    }
+
+    struct MockLicenseTextEgressRepository {
+        consumed_text: RefCell<Box<str>>,
+    }
+
+    impl LicenseTextEgressRepository for MockLicenseTextEgressRepository {
+        fn consume(&self, text: &str) -> Result<(), AnyError> {
+            self.consumed_text.replace(text.into());
+            Ok(())
+        }
+    }
+
     #[test]
     fn create_license_usecase() {
-        unimplemented!();
+        let license_list_ingress_repository = Rc::new(MockLicenseListIngressRepository {});
+        let license_choice_ingress_repository = Rc::new(MockLicenseChoiceIngressRepository {});
+        let license_text_ingress_repository = Rc::new(MockLicenseTextIngressRepository {});
+        let license_text_egress_repository = Rc::new(MockLicenseTextEgressRepository {
+            consumed_text: RefCell::new("".into()),
+        });
+        let usecase = CreateLicenseUseCase::new(
+            license_list_ingress_repository,
+            license_choice_ingress_repository,
+            license_text_ingress_repository,
+            license_text_egress_repository.clone(),
+        );
+        let _result = usecase.execute().expect("Repositories do not return Errs");
+        assert_eq!(
+            license_text_egress_repository.consumed_text.take(),
+            "Name: MIT\nReference: mit-license\n".into()
+        );
     }
 }
