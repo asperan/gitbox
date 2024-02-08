9a22fa0e9bc24b5ab8a89b82ce045e4be3e890e0
Alex Speranza <alex.speranza@studio.unibo.it>
2024-02-08 12:56:23 +0100

    test(application): add test for license controller

--- src/lib/application/controller/license.rs
+++ src/lib/application/controller/license.rs
@@ -86,8 +86,107 @@ impl LicenseController {
 
 #[cfg(test)]
 mod tests {
+    use std::{cell::RefCell, error::Error, fmt::Display, rc::Rc};
+
+    use crate::{
+        application::{
+            controller::{exit_code::ControllerExitCode, license::LicenseController},
+            manager::{
+                license_choice_ingress_manager::LicenseChoiceIngressManager,
+                license_list_ingress_manager::LicenseListIngressManager,
+                license_text_egress_manager::{self, LicenseTextEgressManager},
+                license_text_ingress_manager::LicenseTextIngressManager,
+                message_egress_manager::MessageEgressManager,
+            },
+            options::license::LicenseOptions,
+        },
+        usecase::{license_metadata::LicenseMetadata, type_aliases::AnyError},
+    };
+
+    #[derive(Debug)]
+    struct MockError {}
+    impl Display for MockError {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "Mock error")
+        }
+    }
+    impl Error for MockError {}
+
+    struct MockLicenseChoiceIngressManager {}
+
+    impl LicenseChoiceIngressManager for MockLicenseChoiceIngressManager {
+        fn ask_license<'a>(
+            &self,
+            list: &'a [LicenseMetadata],
+        ) -> Result<&'a LicenseMetadata, AnyError> {
+            if !list.is_empty() {
+                Ok(&list[0])
+            } else {
+                Err(MockError {}.into())
+            }
+        }
+    }
+
+    struct MockLicenseListIngressManager {
+        list: Vec<LicenseMetadata>,
+    }
+    impl LicenseListIngressManager for MockLicenseListIngressManager {
+        fn license_list(&self) -> Result<Box<[LicenseMetadata]>, AnyError> {
+            Ok(self.list.as_slice().into())
+        }
+    }
+
+    struct MockLicenseTextEgressManager {
+        filepath: RefCell<Box<str>>,
+        text: RefCell<Box<str>>,
+    }
+    impl LicenseTextEgressManager for MockLicenseTextEgressManager {
+        fn write_license(&self, filepath: &str, text: &str) -> Result<(), AnyError> {
+            self.filepath.replace(filepath.into());
+            self.text.replace(text.into());
+            Ok(())
+        }
+    }
+
+    struct MockLicenseTextIngressManager {
+        text: Box<str>,
+    }
+    impl LicenseTextIngressManager for MockLicenseTextIngressManager {
+        fn license_text(&self, _license: &LicenseMetadata) -> Result<Box<str>, AnyError> {
+            Ok(self.text.clone())
+        }
+    }
+
+    struct VoidMessageEgressManager {}
+    impl MessageEgressManager for VoidMessageEgressManager {
+        fn output(&self, _message: &str) {}
+        fn error(&self, _error: &str) {}
+    }
+
     #[test]
     fn license_controller() {
-        unimplemented!();
+        let options = LicenseOptions::new("/tmp/test-path");
+        let license_list_ingress_manager = Rc::new(MockLicenseListIngressManager {
+            list: vec![LicenseMetadata::new("MIT", "mit-license")],
+        });
+        let license_choice_ingress_manager = Rc::new(MockLicenseChoiceIngressManager {});
+        let license_text_ingress_manager = Rc::new(MockLicenseTextIngressManager {
+            text: "License text".into(),
+        });
+        let license_text_egress_manager = Rc::new(MockLicenseTextEgressManager {
+            text: RefCell::new("".into()),
+            filepath: RefCell::new("".into()),
+        });
+        let message_egress_manager = Rc::new(VoidMessageEgressManager {});
+        let controller = LicenseController::new(
+            options,
+            license_list_ingress_manager,
+            license_choice_ingress_manager,
+            license_text_ingress_manager,
+            license_text_egress_manager,
+            message_egress_manager,
+        );
+        let result = controller.license();
+        assert!(matches!(result, ControllerExitCode::Ok));
     }
 }
