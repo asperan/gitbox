9470f90d3e11a36875cf843e28e8a55cd61ccc09
Alex Speranza <alex.speranza@studio.unibo.it>
2024-07-04 21:57:13 +0200

    refactor(domain): add checks on prerelease and metadata

--- src/lib/domain/semantic_version.rs
+++ src/lib/domain/semantic_version.rs
@@ -1,5 +1,9 @@
 use std::cmp::Ordering;
 
+use super::error::semantic_version_invariant_error::{
+    InvalidMetadataStringError, InvalidPrereleaseStringError, SemanticVersionInvariantError,
+};
+
 /*
 A [semantic version](https://semver.org/).
 */
@@ -19,14 +23,14 @@ impl SemanticVersion {
         patch: u32,
         prerelease: Option<String>,
         metadata: Option<String>,
-    ) -> SemanticVersion {
-        SemanticVersion {
+    ) -> Result<Self, SemanticVersionInvariantError> {
+        Ok(SemanticVersion {
             major,
             minor,
             patch,
-            prerelease,
-            metadata,
-        }
+            prerelease: Self::check_prerelease(prerelease)?,
+            metadata: Self::check_metadata(metadata)?,
+        })
     }
 
     pub fn major(&self) -> u32 {
@@ -47,6 +51,50 @@ impl SemanticVersion {
 
     pub fn metadata(&self) -> Option<&str> {
         self.metadata.as_deref()
+    }
+
+    fn check_prerelease(
+        prerelease: Option<String>,
+    ) -> Result<Option<String>, SemanticVersionInvariantError> {
+        match prerelease {
+            Some(wrong)
+                if wrong.is_empty()
+                    || wrong.chars().any(|it| {
+                        !(it.is_ascii_digit()
+                            || it.is_ascii_lowercase()
+                            || it.is_ascii_uppercase()
+                            || it == '-')
+                    }) =>
+            {
+                Err(SemanticVersionInvariantError::InvalidPrerelease(
+                    InvalidPrereleaseStringError::new(wrong),
+                ))
+            }
+            None => Ok(None),
+            Some(s) => Ok(Some(s)),
+        }
+    }
+
+    fn check_metadata(
+        metadata: Option<String>,
+    ) -> Result<Option<String>, SemanticVersionInvariantError> {
+        match metadata {
+            Some(wrong)
+                if wrong.is_empty()
+                    || wrong.chars().any(|it| {
+                        !(it.is_ascii_digit()
+                            || it.is_ascii_lowercase()
+                            || it.is_ascii_uppercase()
+                            || it == '-')
+                    }) =>
+            {
+                Err(SemanticVersionInvariantError::InvalidMetadata(
+                    InvalidMetadataStringError::new(wrong),
+                ))
+            }
+            None => Ok(None),
+            Some(s) => Ok(Some(s)),
+        }
     }
 }
 
@@ -86,10 +134,12 @@ impl PartialOrd<SemanticVersion> for SemanticVersion {
 mod tests {
     use std::cmp::Ordering;
 
+    use crate::domain::error::semantic_version_invariant_error::SemanticVersionInvariantError;
+
     use super::SemanticVersion;
 
     fn first_release() -> SemanticVersion {
-        SemanticVersion::new(0, 1, 0, None, None)
+        SemanticVersion::new(0, 1, 0, None, None).expect("Hand-crafted version must be correct")
     }
 
     /// Ordering tests
@@ -104,13 +154,15 @@ mod tests {
     #[test]
     fn less_versions_ordering() {
         let v1 = first_release();
-        let v2 = SemanticVersion::new(1, 0, 0, None, None);
+        let v2 = SemanticVersion::new(1, 0, 0, None, None)
+            .expect("Hand-crafted version must be correct");
         assert_eq!(v1.partial_cmp(&v2) == Some(Ordering::Less), v1 < v2);
     }
 
     #[test]
     fn greater_versions_ordering() {
-        let v1 = SemanticVersion::new(0, 1, 1, None, None);
+        let v1 = SemanticVersion::new(0, 1, 1, None, None)
+            .expect("Hand-crafted version must be correct");
         let v2 = first_release();
         assert_eq!(v1.partial_cmp(&v2) == Some(Ordering::Greater), v1 > v2);
     }
@@ -118,7 +170,8 @@ mod tests {
     #[test]
     fn less_or_equal_versions_ordering() {
         let v1 = first_release();
-        let v2 = SemanticVersion::new(1, 0, 0, None, None);
+        let v2 = SemanticVersion::new(1, 0, 0, None, None)
+            .expect("Hand-crafted version must be correct");
         let v3 = first_release();
 
         let partial_cmp_result = v1.partial_cmp(&v2);
@@ -138,9 +191,11 @@ mod tests {
 
     #[test]
     fn greater_or_equal_versions_ordering() {
-        let v1 = SemanticVersion::new(0, 1, 1, None, None);
+        let v1 = SemanticVersion::new(0, 1, 1, None, None)
+            .expect("Hand-crafted version must be correct");
         let v2 = first_release();
-        let v3 = SemanticVersion::new(0, 1, 1, None, None);
+        let v3 = SemanticVersion::new(0, 1, 1, None, None)
+            .expect("Hand-crafted version must be correct");
         let partial_cmp_result = v1.partial_cmp(&v2);
         assert_eq!(
             partial_cmp_result == Some(Ordering::Greater)
@@ -159,14 +214,65 @@ mod tests {
     #[test]
     fn prerelease_is_less_than_version() {
         let v1 = first_release();
-        let v2 = SemanticVersion::new(0, 1, 0, Some("dev1".to_string()), None);
+        let v2 = SemanticVersion::new(0, 1, 0, Some("dev1".to_string()), None)
+            .expect("Hand-crafted version must be correct");
         assert!(v1 > v2);
     }
 
     #[test]
     fn prereleases_are_ordered_lexicographically() {
-        let v1 = SemanticVersion::new(0, 1, 0, Some("beta1".to_string()), None);
-        let v2 = SemanticVersion::new(0, 1, 0, Some("alpha3".to_string()), None);
+        let v1 = SemanticVersion::new(0, 1, 0, Some("beta1".to_string()), None)
+            .expect("Hand-crafted version must be correct");
+        let v2 = SemanticVersion::new(0, 1, 0, Some("alpha3".to_string()), None)
+            .expect("Hand-crafted version must be correct");
         assert!(v2 < v1);
+    }
+
+    // Invariant tests
+    #[test]
+    fn prerelease_invariant_if_empty() {
+        let v = SemanticVersion::new(0, 1, 0, Some("".to_string()), None);
+        assert!(matches!(
+            v,
+            Err(SemanticVersionInvariantError::InvalidPrerelease(_))
+        ));
+    }
+
+    #[test]
+    fn prerelease_invariant_with_wrong_char() {
+        let v = SemanticVersion::new(0, 1, 0, Some("dev_1".to_string()), None);
+        assert!(matches!(
+            v,
+            Err(SemanticVersionInvariantError::InvalidPrerelease(_))
+        ));
+    }
+
+    #[test]
+    fn prerelease_invariant_correct() {
+        let v = SemanticVersion::new(0, 1, 0, Some("dev1".to_string()), None);
+        assert!(matches!(v, Ok(_)));
+    }
+    #[test]
+    fn metadata_invariant_if_empty() {
+        let v = SemanticVersion::new(0, 1, 0, None, Some("".to_string()));
+        assert!(matches!(
+            v,
+            Err(SemanticVersionInvariantError::InvalidMetadata(_))
+        ));
+    }
+
+    #[test]
+    fn metadata_invariant_with_wrong_char() {
+        let v = SemanticVersion::new(0, 1, 0, None, Some("sha_date".to_string()));
+        assert!(matches!(
+            v,
+            Err(SemanticVersionInvariantError::InvalidMetadata(_))
+        ));
+    }
+
+    #[test]
+    fn metadata_invariant_correct() {
+        let v = SemanticVersion::new(0, 1, 0, None, Some("sha-date".to_string()));
+        assert!(matches!(v, Ok(_)));
     }
 }
