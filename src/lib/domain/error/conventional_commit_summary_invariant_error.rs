7a00f4e616fced811c88e8e3c3f05cab458e4977
Alex Speranza <alex.speranza@studio.unibo.it>
2024-07-05 21:48:56 +0200

    refactor(domain): add error for invariants in conventional commit summary

---
+++ src/lib/domain/error/conventional_commit_summary_invariant_error.rs
@@ -0,0 +1,99 @@
+use std::{error::Error, fmt::Display};
+
+#[derive(Debug)]
+pub enum ConventionalCommitSummaryInvariantError {
+    InvalidType(InvalidTypeError),
+    InvalidScope(InvalidScopeError),
+    InvalidSummary(InvalidSummaryError),
+}
+
+impl Display for ConventionalCommitSummaryInvariantError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "invariant violated: {}", self.source().expect("The source is always present"))
+    }
+}
+
+impl Error for ConventionalCommitSummaryInvariantError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            Self::InvalidType(e) => Some(e),
+            Self::InvalidScope(e) => Some(e),
+            Self::InvalidSummary(e) => Some(e),
+        }
+    }
+}
+
+impl From<InvalidTypeError> for ConventionalCommitSummaryInvariantError {
+    fn from(value: InvalidTypeError) -> Self {
+        Self::InvalidType(value)
+    }
+}
+
+impl From<InvalidScopeError> for ConventionalCommitSummaryInvariantError {
+    fn from(value: InvalidScopeError) -> Self {
+        Self::InvalidScope(value)
+    }
+}
+
+impl From<InvalidSummaryError> for ConventionalCommitSummaryInvariantError {
+    fn from(value: InvalidSummaryError) -> Self {
+        Self::InvalidSummary(value)
+    }
+}
+
+#[derive(Debug)]
+pub struct InvalidTypeError {
+    wrong_type: String,
+}
+
+impl InvalidTypeError {
+    pub fn new(wrong_type: String) -> Self {
+        InvalidTypeError { wrong_type }
+    }
+}
+
+impl Display for InvalidTypeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "invalid type '{}'", self.wrong_type)
+    }
+}
+
+impl Error for InvalidTypeError {}
+
+#[derive(Debug)]
+pub struct InvalidScopeError {
+    wrong_scope: String,
+}
+
+impl InvalidScopeError {
+    pub fn new(wrong_scope: String) -> Self {
+        InvalidScopeError { wrong_scope }
+    }
+}
+
+impl Display for InvalidScopeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "invalid scope '{}'", self.wrong_scope)
+    }
+}
+
+impl Error for InvalidScopeError {}
+
+#[derive(Debug)]
+pub struct InvalidSummaryError {
+    wrong_summary: String,
+}
+
+impl InvalidSummaryError {
+    pub fn new(wrong_summary: String) -> Self {
+        InvalidSummaryError { wrong_summary }
+    }
+}
+
+impl Display for InvalidSummaryError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "invalid summary '{}'", self.wrong_summary)
+    }
+}
+
+impl Error for InvalidSummaryError {}
