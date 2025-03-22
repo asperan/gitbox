7669dd2f05b47cdb3cbe551b86b7a2c089c5cdce
Alex Speranza <alex.speranza@studio.unibo.it>
2025-03-22 19:32:06 +0100

    fix(infrastructure): change git tag cleanup mode to whitespace to allow '#' characters in formats

--- src/lib/infrastructure/interface/git_cli.rs
+++ src/lib/infrastructure/interface/git_cli.rs
@@ -148,6 +148,7 @@ impl InitEgressManager for GitCli {
 impl TagEgressManager for GitCli {
     fn create_tag(&self, label: &str, message: Option<&str>, sign: bool) -> Result<(), AnyError> {
         let mut args = vec!["tag", label];
+        args.push("--cleanup=whitespace");
         args.push("-m");
         match message {
             Some(s) => {
